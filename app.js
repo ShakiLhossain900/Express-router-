3d01072c982c95486880614a0d218ac21f8cdb29
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2024-02-10 18:50:04 +0600

    adding

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const app = express();
 
-const 
+const adminRoutes = require('./Routers/admin')
 
 app.use(bodyParser.urlencoded({ extended:flase}));
 
-app.use();
+app.use(adminRoutes);
 
-app.use((req, res, next) =>{
+app.use('/',(req, res, next) =>{
     res.send('<h1>Hello i am from express!</h1>')
 });
 
