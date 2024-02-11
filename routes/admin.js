92fc0dd202b7401524824976351b8af7a96e78f1
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2024-02-11 23:32:31 +0600

    adding some comment

--- routes/admin.js
+++ routes/admin.js
@@ -2,12 +2,14 @@ const express = require("express");
 
 const router = express.Router();
 
+//  /admin/add-product => GET request user input dibe /admin akhane check korbe na karon ata app y /admin hoye akhan thake /add-product will only take
 router.get("/add-product", (req, res, next) => {
   res.send(
     '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
   );
 });
 
+//  /admin/add-product => Post request user ouput pabe like fb post er moto
 router.post("/add-product", (req, res, next) => {
   console.log(req.body);   //user get y jei input ta diyece amara post y ata pabu
   res.redirect("/");
