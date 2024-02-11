227b7c20439d0df2031ef1c3268141b24289b684
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2024-02-11 23:25:51 +0600

    adding admin redirect

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,17 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-
-router.get('/add-product',(req, res, next) => {
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+router.get("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  );
 });
 
-router.post('/add-product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-})
+router.post("/add-product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
 
 module.exports = router;
 
