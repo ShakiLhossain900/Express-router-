cdbb064a98803a512b574ca9b55e2e3600cfdcaa
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2024-02-11 23:56:53 +0600

    connecting HTML

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,11 @@
+const path = require('path');
 const express = require("express");
 
 const router = express.Router();
 
 //  /admin/add-product => GET request user input dibe /admin akhane check korbe na karon ata app y /admin hoye akhan thake /add-product will only take
 router.get("/add-product", (req, res, next) => {
-  res.send  
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));  //__dirname get the file of this folder diye bojay
 });
 
 //  /admin/add-product => Post request user ouput pabe like fb post er moto
