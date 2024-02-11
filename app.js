51a97b384d3b41a571a4ce25a919354456c4086b
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2024-02-11 23:16:48 +0600

    adding new error

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false}));
 
-app.use(adminRoutes);
+app.use('/admin',adminRoutes); //only /admin dile adim route file jabe
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>page is not found</h1>')
+    res.status(404).send('<h1>page is not found</h1>');  //dummy path dile like /sdfdhsfjk
 });
 
 
