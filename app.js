7f48ba4027a971bbeda98d5fbbb5aded82a9f969
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2024-02-12 23:18:38 +0600

    adding the css

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false}));
+app.use(express.static(path.join(__dirname,'public')));
 
 app.use('/admin',adminRoutes); //only /admin dile adim route file jabe
 app.use(shopRoutes);
