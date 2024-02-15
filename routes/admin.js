0884d56c890038c535450be8c16e4f559ffe4916
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2024-02-15 17:28:49 +0600

    adding

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,7 @@ const products = []; //
 //  /admin/add-product => GET request user input dibe /admin akhane check korbe na karon ata app y /admin hoye akhan thake /add-product will only take
 router.get("/add-product", (req, res, next) => {
   //res.sendFile(path.join(rootDir, 'views', 'add-product.html'));  //__dirname get the file of this folder diye bojay
-   res.render('add-product', {pageTitle : 'Add Product', path:'/admin/add-porduct});
+   res.render('add-product', {pageTitle : 'Add Product', path :'/admin/add-product' }); //amara view the path pathabu amader running path ki ache dekar jonnu
 });
 
 //  /admin/add-product => Post request user ouput pabe like fb post er moto
--- routes/shop.js
+++ routes/shop.js
@@ -1,22 +1,18 @@
-const path = require('path');
-const express = require('express');
-const rootDir = require('../util/path');
-const adminData = require('./admin');
-
-
+const path = require("path");
+const express = require("express");
+const rootDir = require("../util/path");
+const adminData = require("./admin");
 
 const router = express.Router();
 
-router.get('/',(req, res, next) =>{
-    // console.log(adminData.products);
-    // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
- 
-    // for the pug express js kiso change korece 
-    const products = adminData.products;
-    res.render('shop', {prods : products, docTitle : 'Shop'}); //ata auto shop.pug ke niye nibe output y
+router.get("/", (req, res, next) => {
+  // console.log(adminData.products);
+  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 
-
+  // for the pug express js kiso change korece
+  const products = adminData.products;
+  res.render("shop", { prods: products, docTitle: "Shop", path: "/" }); //ata auto shop.pug ke niye nibe output y
 });
 
 module.exports = router;
- 
+
