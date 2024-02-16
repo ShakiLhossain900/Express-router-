3ef5476b806ff6f45b8ad8a30d50f66e1824633c
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2024-02-16 23:36:06 +0600

    adding

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
-const path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const expressHbs = require("express-handlebars");
 
 const app = express();
 
-app.engine('hbs', expressHbs())
-app.set('view engine', 'hbs'); //Note: The view engine cache does not cache the contents of the template’s output, 
-app.set('views', 'views');  //directory fo the folder (find)
+//app.engine('hbs', expressHbs())
+app.set("view engine", "ejs"); //Note: The view engine cache does not cache the contents of the template’s output,
+app.set("views", "views"); //directory fo the folder (find)
 
-const admindata = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const admindata = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use(bodyParser.urlencoded({ extended: false}));
-app.use(express.static(path.join(__dirname,'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin',admindata.routes); //only /admin dile adim route file jabe
+app.use("/admin", admindata.routes); //only /admin dile adim route file jabe
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    //res.sendFile(path.join(__dirname, './', 'views', '404.html'));
-    res.status(404).render('404',{pageTitle: 'Page is Not Found'});
+  //res.sendFile(path.join(__dirname, './', 'views', '404.html'));
+  res.status(404).render("404", { pageTitle: "Page is Not Found" });
 });
 
-
 app.listen(3000);
-
 
 //pug one or two option er jonnu use kore handlebar amara pug ke dynamic vabe use korte parbeu
 //pug y onk dependancy ache jetay amara reuse korte code ke onk jamle and html er virtor
