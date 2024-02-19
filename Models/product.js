cc5a211f57547fb9a07d5db45cd297824f95e8aa
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2024-02-19 15:44:19 +0600

    import the file sytem

--- Models/product.js
+++ Models/product.js
@@ -1,14 +1,17 @@
-const products =[];
+// const products =[];
+const fs = require('fs');
+const path = require('path');
 
 module.exports = class Product{
     constructor (t){
         this.title = t;
     }
     save(){
-        products.push(this);
+        // products.push(this);
+
     }
 
    static fetchAll() {
-      return this.products;
+      return products;
     }
 }
