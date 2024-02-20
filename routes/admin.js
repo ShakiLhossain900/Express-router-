3dc307d1827ce2765e74228f5b088a6bdce4167c
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2024-02-20 21:47:15 +0600

    working on view js

--- routes/admin.js
+++ routes/admin.js
@@ -38,27 +38,47 @@
 
 // the admin copy from max
 
+// const path = require('path');
+
+// const express = require('express');
+
+// const productsCtrollers =require('../controllers/products');
+
+// // const rootDir = require('../util/path'); //controller add korar karone ata lagbe na
+
+
+// const router = express.Router();
+
+
+
+// // /admin/add-product => GET
+// router.get('/add-product', productsCtrollers.getAddProduct); // onlye reference fuction/
+
+// // /admin/add-product => POST
+// router.post('/add-product', productsCtrollers.postAddProduct);
+
+// // exports.routes = router;
+// // exports.products = products;
+
+// module.exports = router;
+
+
+
 const path = require('path');
 
 const express = require('express');
 
-const productsCtrollers =require('../controllers/products');
-
-// const rootDir = require('../util/path'); //controller add korar karone ata lagbe na
-
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-
-
 // /admin/add-product => GET
-router.get('/add-product', productsCtrollers.getAddProduct); // onlye reference fuction/
+router.get('/add-product', adminController.getAddProduct);
+
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', productsCtrollers.postAddProduct);
-
-// exports.routes = router;
-// exports.products = products;
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
-
--- routes/shop.js
+++ routes/shop.js
@@ -19,17 +19,39 @@
 
 
 //shop js copy from max
+// const path = require('path');
+
+// const express = require('express');
+
+// const productsCtroller = require('../controllers/products');
+
+// // const rootDir = require('../util/path');  //using the logic in conttroler tai amader agolo lagbe na
+// // const adminData = require('./admin');
+
+// const router = express.Router();
+
+// router.get('/', productsCtroller.getProducts);
+
+// module.exports = router;
+
+
+
 const path = require('path');
 
 const express = require('express');
 
-const productsCtroller = require('../controllers/products');
-
-// const rootDir = require('../util/path');  //using the logic in conttroler tai amader agolo lagbe na
-// const adminData = require('./admin');
+const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
-router.get('/', productsCtroller.getProducts);
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
