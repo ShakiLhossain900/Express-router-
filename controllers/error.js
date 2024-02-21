540f4d94c61f8b11bef4091747ab627e0f51c53f
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2024-02-21 19:48:18 +0600

    adding

--- controllers/error.js
+++ controllers/error.js
@@ -1,4 +1,4 @@
 exports.get404= (req, res, next) => {
     //res.sendFile(path.join(__dirname, './', 'views', '404.html'));
-    res.status(404).render("404", { pageTitle: "Page is Not Found" });
+    res.status(404).render("404", { pageTitle: "Page is Not Found", path: '/404'});
   }
