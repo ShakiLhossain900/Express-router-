02ce380d3578ddcf714394668435e40b01eed64a
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2024-02-23 18:33:28 +0600

    code has some error to never control others book details

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,50 +1,102 @@
-const Product = require("../models/product");
+// const Product = require("../models/product");
 
-exports.getProducts = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render("shop/product-list", {
-      prods: products,
-      pageTitle: "All Products",
-      path: "/products",
+// exports.getProducts = (req, res, next) => {
+//   Product.fetchAll((products) => {
+//     res.render("shop/product-list", {
+//       prods: products,
+//       pageTitle: "All Products",
+//       path: "/products",
+//     });
+//   });
+// };
+
+// exports.getProduct=(req,res,next) => {
+//   const prodId = req.params.productId;
+//   Product.findById(prodId,product =>{
+//     console.log(product);
+//   })
+//   res.redirect('/');
+// }
+
+// exports.getIndex = (req, res, next) => {
+//   Product.fetchAll((products) => {
+//     res.render("shop/index", {
+//       prods: products,
+//       pageTitle: "Shop",
+//       path: "/",
+//     });
+//   });
+// };
+
+// exports.getCart = (req, res, next) => {
+//   res.render("shop/cart", {
+//     path: "/cart",
+//     pageTitle: "Your Cart",
+//   });
+// };
+
+// exports.getOrders = (req, res, next) => {
+//   res.render("shop/orders", {
+//     path: "/orders",
+//     pageTitle: "Your Orders",
+//   });
+// };
+
+// exports.getCheckout = (req, res, next) => {
+//   res.render("shop/checkout", {
+//     path: "/checkout",
+//     pageTitle: "Checkout",
+//   });
+// };
+
+
+//COPIED CODE FOR THE PRODUCT DETAILS BUT OTEHRS DETAILS NOT WORKIING THAT'S WHY COPY CODE 
+
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
+module.exports = class Product {
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {
+    this.id = Math.random().toString();
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
     });
-  });
-};
+  }
 
-exports.getProduct=(req,res,next) => {
-  const prodId = req.params.productId;
-  Product.findById(prodId,product =>{
-    console.log(product);
-  })
-  res.redirect('/');
-}
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
 
-exports.getIndex = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render("shop/index", {
-      prods: products,
-      pageTitle: "Shop",
-      path: "/",
+  static findById(id, cb) {
+    getProductsFromFile(products => {
+      const product = products.find(p => p.id === id);
+      cb(product);
     });
-  });
-};
-
-exports.getCart = (req, res, next) => {
-  res.render("shop/cart", {
-    path: "/cart",
-    pageTitle: "Your Cart",
-  });
-};
-
-exports.getOrders = (req, res, next) => {
-  res.render("shop/orders", {
-    path: "/orders",
-    pageTitle: "Your Orders",
-  });
-};
-
-exports.getCheckout = (req, res, next) => {
-  res.render("shop/checkout", {
-    path: "/checkout",
-    pageTitle: "Checkout",
-  });
+  }
 };
