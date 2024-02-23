4f674cf165fc08fda0ee3dff10d88033595333b1
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2024-02-23 18:46:30 +0600

    error already solved

--- Models/product.js
+++ Models/product.js
@@ -39,111 +39,17 @@
 //   }
 // };
 
-// const fs = require("fs");
-// const path = require("path");
 
-// module.exports = class Product {
-//   constructor(t) {
-//     this.title = t;
-//   }
-
-//   save() {
-//     const p = path.join(
-//       path.dirname(process.mainModule.filename),
-//       "data",
-//       "products.json"
-//     );
-//     fs.readFile(p, (err, fileContent) => {
-//       let products = [];
-//       if (!err) {
-//         products = JSON.parse(fileContent);
-//       }
-//       products.push(this);
-//       fs.writeFile(p, JSON.stringify(products), (err) => {
-//         console.log(err);
-//       });
-//     });
-//   }
-
-//   static fetchAll(cb) {
-//     const p = path.join(
-//       path.dirname(process.mainModule.filename),
-//       "data",
-//       "products.json"
-//     );
-//     fs.readFile(p, (err, fileContent) => {
-//       if (err) {
-//         cb([]);
-//       }
-//       cb(JSON.parse(fileContent));
-//     });
-//   }
-// };
-
-// const fs = require("fs");
-// const path = require("path");
-
-// const p = path.join(
-//   path.dirname(process.mainModule.filename),
-//   "data",
-//   "products.json"
-// );
-
-// const getProductsFromFile = (cb) => {
-//   fs.readFile(p, (err, fileContent) => {
-//     if (err) {
-//       cb([]);
-//     } else {
-//       cb(JSON.parse(fileContent));
-//     }
-//   });
-// };
-
-// module.exports = class Product {
-//   constructor(title, imageUrl, description, price) {
-//     this.title = title;
-//     this.imageUrl = imageUrl;
-//     this.description = description;
-//     this.price = price;
-//   }
-
-//   //save add to the database
-//   save() {
-//     this.id = Math.random().toString(); //we create real products  unique id
-//     getProductsFromFile((products) => {
-//       products.push(this);
-//       fs.writeFile(p, JSON.stringify(products), (err) => {
-//         console.log(err);
-//       });
-//     });
-//   }
-
-//   static fetchAll(cb) {
-//     getProductsFromFile(cb);
-//   }
-
-//   static findById(id, cb) {
-//     getProductsFromFile((products) => {
-//       const product = products.find(p => p.id === id);
-//       cb(product);
-//     });
-//   }
-// };
-
-
-
-//COPIED CODE FOR THE PRODUCT DETAILS BUT OTEHRS DETAILS NOT WORKIING THAT'S WHY COPY CODE 
-
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 const p = path.join(
   path.dirname(process.mainModule.filename),
-  'data',
-  'products.json'
+  "data",
+  "products.json"
 );
 
-const getProductsFromFile = cb => {
+const getProductsFromFile = (cb) => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
       cb([]);
@@ -161,11 +67,12 @@ module.exports = class Product {
     this.price = price;
   }
 
+  //save add to the database
   save() {
-    this.id = Math.random().toString();
-    getProductsFromFile(products => {
+    this.id = Math.random().toString(); //we create real products  unique id
+    getProductsFromFile((products) => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
+      fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
       });
     });
@@ -176,10 +83,9 @@ module.exports = class Product {
   }
 
   static findById(id, cb) {
-    getProductsFromFile(products => {
+    getProductsFromFile((products) => {
       const product = products.find(p => p.id === id);
       cb(product);
     });
   }
 };
-//AFTER RUN THE CODE PRODUCT PRIVIIOUS CODE WAS OK
