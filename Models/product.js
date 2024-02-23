7761e7de6f610a6249ddea049c48c33008cfcd81
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2024-02-23 18:34:29 +0600

    code has some error to never control others book details

--- Models/product.js
+++ Models/product.js
@@ -80,16 +80,70 @@
 //   }
 // };
 
-const fs = require("fs");
-const path = require("path");
+// const fs = require("fs");
+// const path = require("path");
+
+// const p = path.join(
+//   path.dirname(process.mainModule.filename),
+//   "data",
+//   "products.json"
+// );
+
+// const getProductsFromFile = (cb) => {
+//   fs.readFile(p, (err, fileContent) => {
+//     if (err) {
+//       cb([]);
+//     } else {
+//       cb(JSON.parse(fileContent));
+//     }
+//   });
+// };
+
+// module.exports = class Product {
+//   constructor(title, imageUrl, description, price) {
+//     this.title = title;
+//     this.imageUrl = imageUrl;
+//     this.description = description;
+//     this.price = price;
+//   }
+
+//   //save add to the database
+//   save() {
+//     this.id = Math.random().toString(); //we create real products  unique id
+//     getProductsFromFile((products) => {
+//       products.push(this);
+//       fs.writeFile(p, JSON.stringify(products), (err) => {
+//         console.log(err);
+//       });
+//     });
+//   }
+
+//   static fetchAll(cb) {
+//     getProductsFromFile(cb);
+//   }
+
+//   static findById(id, cb) {
+//     getProductsFromFile((products) => {
+//       const product = products.find(p => p.id === id);
+//       cb(product);
+//     });
+//   }
+// };
+
+
+
+//COPIED CODE FOR THE PRODUCT DETAILS BUT OTEHRS DETAILS NOT WORKIING THAT'S WHY COPY CODE 
+
+const fs = require('fs');
+const path = require('path');
 
 const p = path.join(
   path.dirname(process.mainModule.filename),
-  "data",
-  "products.json"
+  'data',
+  'products.json'
 );
 
-const getProductsFromFile = (cb) => {
+const getProductsFromFile = cb => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
       cb([]);
@@ -107,12 +161,11 @@ module.exports = class Product {
     this.price = price;
   }
 
-  //save add to the database
   save() {
-    this.id = Math.random().toString(); //we create real products  unique id
-    getProductsFromFile((products) => {
+    this.id = Math.random().toString();
+    getProductsFromFile(products => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
+      fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
       });
     });
@@ -123,7 +176,7 @@ module.exports = class Product {
   }
 
   static findById(id, cb) {
-    getProductsFromFile((products) => {
+    getProductsFromFile(products => {
       const product = products.find(p => p.id === id);
       cb(product);
     });
