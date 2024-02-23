07221101f94677c07f0a4726a91696580d28a3e4
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2024-02-23 18:31:22 +0600

    code has some error to never control others book details

--- Models/product.js
+++ Models/product.js
@@ -80,18 +80,16 @@
 //   }
 // };
 
-
-
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 const p = path.join(
   path.dirname(process.mainModule.filename),
-  'data',
-  'products.json'
+  "data",
+  "products.json"
 );
 
-const getProductsFromFile = cb => {
+const getProductsFromFile = (cb) => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
       cb([]);
@@ -112,9 +110,9 @@ module.exports = class Product {
   //save add to the database
   save() {
     this.id = Math.random().toString(); //we create real products  unique id
-    getProductsFromFile(products => {
+    getProductsFromFile((products) => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
+      fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
       });
     });
@@ -122,5 +120,12 @@ module.exports = class Product {
 
   static fetchAll(cb) {
     getProductsFromFile(cb);
+  }
+
+  static findById(id, cb) {
+    getProductsFromFile((products) => {
+      const product = products.find(p => p.id === id);
+      cb(product);
+    });
   }
 };
